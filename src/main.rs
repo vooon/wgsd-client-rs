6ea95074d31b6205b1296bf6404626b9fc41980a
Vladimir Ermakov <vooon341@gmail.com>
2025-07-05 08:55:11 +0200

    init cli opts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use argh::FromArgs;
+
+#[derive(FromArgs)]
+/// WireGuard Service Discovery client
+struct Cli {
+    #[argh(option, short = 'd')]
+    /// network interface name
+    device: String,
+
+    #[argh(option, short = 's')]
+    /// DNS server to use, IP:PORT
+    dns: String,
+
+    #[argh(option, short = 'n')]
+    /// base DNS Zone to lookup
+    zone: String,
+
+    #[argh(switch)]
+    /// debug logging
+    debug: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli: Cli = argh::from_env();
+
+    println!("Hello, world! {}", cli.device);
 }
